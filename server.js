e3df797d421be664e75e9755818e10bb52fa68f9
Dheya Madhani <dheyam@gmail.com>
2022-08-30 16:07:20 -0400

    server complete

--- server.js
+++ server.js
@@ -41,7 +41,12 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
 
-
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end(fs.readFile('/Users/joe/test.txt', 'utf8'))
+  })
+  
 
 
 
